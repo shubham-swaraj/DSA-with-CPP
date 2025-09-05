b8a0a97f6f4e446545d840fe367d0c794d7b0ca0
Shubham swaraj <1styearatlpu@gmail.com>
2025-09-05 09:22:59 +0530

    Learning Datatype

---
+++ datatype.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	float b;
+	char c;
+	string s;
+	cout<<"Enter a number: ";
+	cin>>a;
+	cout<<"Enter a float value: ";
+	cin>>b;
+	cout<<"Enter a character: ";
+	cin>>c;
+	cout<<"Enter a string: ";
+	cin>>s;
+	cout<<a+b<<" is "<<c<<" "<<s;
+}
